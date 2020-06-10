96250a233a05af578ca4f00cf13b96ace0f271fc
Hunter <hmh84@drexel.edu>
2020-06-10 14:08:11 -0400

    :briefcase: Minor Tweaks :wrench:
    Made hero img responsive w/ width 100% & JS to update its container

--- case-study/js/main.js
+++ case-study/js/main.js
@@ -1,6 +1,23 @@
 vid = document.getElementById("vid");
 vid.disablePictureInPicture = true;
 
+const hero_itself = document.querySelector('#hero');
+const hero_img = document.querySelector('#hero-img');
+
+window.onresize = headerCheck;
+window.onload = initCheck;
+
+function initCheck() {
+    headerCheck();
+    window.scrollTo(0, 0);
+}
+
+function headerCheck() {
+    var x = hero_img.clientHeight;
+    hero_itself.style.height = x+'px';
+    console.log(hero_itself.style.height);
+}
+
 // Micro Interaction
 
 const trash_icon = document.querySelectorAll('.icon-trash');
